506cec31fd1afca4e14d8e8717d6433dabf5ec0e
Sam Brockie <sgb39@cam.ac.uk>
2022-10-18 15:15:27 +0200

    Remove superfluous types from hypothesis-based tests

--- tests/test_symbol.py
+++ tests/test_symbol.py
@@ -1,5 +1,6 @@
 """Tests for the drop-in replacement of SymPy's `Symbol` class."""
 
+
 import pytest
 from hypothesis import given, strategies as st
 
@@ -15,7 +16,7 @@ def test_symbol_str_and_repr():
 def test_symbol_caching():
     a = egraph.Symbol("a")
     assert a is egraph.Symbol("a")
-    assert egraph.Symbol("a") is egraph.Symbol("a")
+    assert egraph.Symbol("b") is egraph.Symbol("b")
 
 
 @given(st.integers() | st.floats())
@@ -24,13 +25,19 @@ def test_number_symbols_invalid(value):
         _ = egraph.Symbol(value)
 
 
+def test_integer_caching():
+    zero = egraph.Integer(0)
+    assert zero is egraph.Integer(0)
+    assert egraph.Integer(1) is egraph.Integer(1)
+
+
 def test_integer_str_repr():
     zero = egraph.Integer(0)
     assert str(zero) == "0"
     assert repr(zero) == "Integer(0)"
 
 
-@given(st.complex_numbers() | st.floats() | st.fractions())
-def test_non_integer_integers_invalid(value):
+@given(st.floats())
+def test_floating_point_integers_invalid(value):
     with pytest.raises(ValueError):
         _ = egraph.Integer(value)
